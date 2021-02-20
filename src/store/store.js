0b2d852264cadf6b7f55eacf6249566356500d84
catalin.rosu <d9f10ee5c4267b63b9801077a96e4f379057133a>
2021-02-20 21:48:05 +0200

    add new page Acasa

---
+++ src/store/store.js
@@ -0,0 +1,10 @@
+import {createStore,combineReducers} from "redux";
+
+const reducers = {
+
+};
+
+const rootReducer = combineReducers(reducers);
+
+
+export const configureStore = () =>createStore(rootReducer)
